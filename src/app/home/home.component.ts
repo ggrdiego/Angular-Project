ae749caac259c56a38a20393492e798c5126fa22
dgeis <ggr.diego@gmail.com>
2024-12-21 18:23:36 -0300

    Adicionando interação http com json server

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,13 +11,13 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filtrar por cidades"  #filter/>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Procurar</button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of  filteredLocationList"
         [housingLocation]="housingLocation"
       ></app-housing-location>
     </section>
@@ -35,18 +35,23 @@ export class HomeComponent {
 
   constructor() {
 
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
 
-    }
+    });
+  }
 
     filterResults(text: string){
       if(!text){
         this.filteredLocationList = this.housingLocationList;
         return;
       }
-    }
+
+      this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+      );
 
   };
-
+}
 
